529c22573753752d33e5a5bd69c312b614dcb368
skrewbar <skurua1244@gmail.com>
2025-07-13 21:23:10 +0900

    ✨ 언어 api 작성

---
+++ src/app/api/languages/route.ts
@@ -0,0 +1,32 @@
+import { Language } from "@/types"
+
+const languages: Language[] = [
+  {
+    name: "C++23",
+    compile:
+      "g++ Main.cpp -o Main -O2 -Wall -lm -static -std=gnu++23 -DONLINE_JUDGE",
+    execute: "./Main",
+    version: "g++ (GCC) 14.2.0",
+  },
+  {
+    name: "Python3",
+    compile: `python3 -W ignore -c "import py_compile; py_compile.compile(r'Main.py')"`,
+    execute: "python3 -W ignore Main.py",
+    version: "Python 3.13",
+  },
+  {
+    name: "Java 15",
+    compile:
+      "javac -release 15 -J-Xms1024m -J-Xmx1920m -J-Xss512m -encoding UTF-8 Main.java",
+    execute:
+      "java -Xms1024m -Xmx1920m -Xss512m -Dfile.encoding=UTF-8 -XX:+UseSerialGC -DONLINE_JUDGE=1 Main",
+    version: `openjdk version "16.0.1" 2021-04-20`,
+  },
+]
+
+export async function GET() {
+  return new Response(JSON.stringify(languages), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  })
+}
