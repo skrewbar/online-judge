206a34f215e3b23d48d65491922afb5516865473
skrewbar <skurua1244@gmail.com>
2025-07-15 00:55:06 +0900

    ✨ 로딩 이미지 컴포넌트 추가

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,6 +11,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { LoadingImage } from "@/components/ui/loading-image"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 
@@ -116,26 +117,7 @@ export default function Page() {
             <Button form="register" type="submit">
               {isLoading ? (
                 <span className="flex items-center justify-center gap-2">
-                  <svg
-                    className="animate-spin h-4 w-4 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    />
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-                    />
-                  </svg>
+                  <LoadingImage className="h-4 w-4" />
                   가입 중...
                 </span>
               ) : (
