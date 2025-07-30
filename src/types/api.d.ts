465db9ee20c17d9caf97301cd5618340a72aea76
skrewbar <skurua1244@gmail.com>
2025-07-30 12:43:24 +0900

    🔥 회원가입 api 제거

--- src/types/api.d.ts
+++
@@ -1,8 +0,0 @@
-type RegisterField = "handle" | "email" | "password" | "pwcheck" | "form"
-export interface RegisterResponse {
-  success: boolean
-  error?: {
-    message: string
-    field: RegisterField
-  }
-}
