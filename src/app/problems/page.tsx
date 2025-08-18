940fbced2395435e3147688e910f7618a2f11876
skrewbar <skurua1244@gmail.com>
2025-08-18 23:31:39 +0900

    ✨ 문제 목록 페이지네이션 구현

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -9,17 +9,40 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+const disabledStyle = "pointer-events-none opacity-50"
 interface PageProps {
   searchParams: Promise<{
-    page?: number
+    page?: string
   }>
 }
 
+const pageSize = 10
+
 export default async function Page({ searchParams }: PageProps) {
   const params = await searchParams
-  const page = params.page ?? 1
+  const page = parseInt(params.page ?? "1")
 
-  const problems = await getPaginatedProblems(page, 10)
+  const { problems, totalCount } = await getPaginatedProblems(page, pageSize)
+
+  const totalPages = Math.ceil(totalCount / pageSize)
+
+  const prevPages = []
+  if (page - 2 > 0) prevPages.push(page - 2)
+  if (page - 1 > 0) prevPages.push(page - 1)
+
+  const nextPages = []
+  if (page + 1 <= totalPages) nextPages.push(page + 1)
+  if (page + 2 <= totalPages) nextPages.push(page + 2)
 
   return (
     <main className="flex justify-center">
@@ -42,6 +65,54 @@ export default async function Page({ searchParams }: PageProps) {
             ))}
           </TableBody>
         </Table>
+
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                href={page === 1 ? "#" : `/problems?page=${page - 1}`}
+                className={page === 1 ? disabledStyle : undefined}
+              />
+            </PaginationItem>
+            {page > 3 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+            {prevPages.map((prevPage) => (
+              <PaginationItem key={prevPage}>
+                <PaginationLink href={`/problems?page=${prevPage}`}>
+                  {prevPage}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+
+            {nextPages.map((nextPage) => (
+              <PaginationItem key={nextPage}>
+                <PaginationLink href={`/problems?page=${nextPage}`}>
+                  {nextPage}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+            {page + 2 < totalPages && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+            <PaginationItem>
+              <PaginationNext
+                href={page === totalPages ? "#" : `/problems?page=${page + 1}`}
+                className={page === totalPages ? disabledStyle : undefined}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </div>
     </main>
   )
