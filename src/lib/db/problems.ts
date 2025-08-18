a16a6b961ba6272596622152117dd6041365524d
skrewbar <skurua1244@gmail.com>
2025-08-18 23:31:26 +0900

    ✨ getPaginatedProblems가 총 문제 수도 반환하도록 변경

--- src/lib/db/problems.ts
+++ src/lib/db/problems.ts
@@ -1,28 +1,36 @@
 import { prisma } from "@/lib/prisma"
 
 export async function getPaginatedProblems(page: number, pageSize: number) {
-  return await prisma.problem.findMany({
-    where: {
-      isPublic: true,
-    },
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-    orderBy: {
-      id: "asc",
-    },
-    select: {
-      id: true,
-      difficulty: true,
-      title: true,
-      isSpecialJudge: true,
-      isInteractive: true,
-      isSubtask: true,
-      _count: {
-        select: {
-          solvedUsers: true,
-          submissions: true,
+  const [problems, totalCount] = await Promise.all([
+    prisma.problem.findMany({
+      where: {
+        isPublic: true,
+      },
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      orderBy: {
+        id: "asc",
+      },
+      select: {
+        id: true,
+        difficulty: true,
+        title: true,
+        isSpecialJudge: true,
+        isInteractive: true,
+        isSubtask: true,
+        _count: {
+          select: {
+            solvedUsers: true,
+            submissions: true,
+          },
         },
       },
-    },
-  })
+    }),
+
+    prisma.problem.count({
+      where: { isPublic: true },
+    }),
+  ])
+
+  return { problems, totalCount }
 }
