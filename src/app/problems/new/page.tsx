e7d292a9b96cf36c8b1f5377c0fa9e86225a8107
skrewbar <skurua1244@gmail.com>
2025-08-19 17:27:10 +0900

    🔒️ admin이 아니면 문제 생성하지 못하도록 변경

--- src/app/problems/new/page.tsx
+++ src/app/problems/new/page.tsx
@@ -1,10 +1,14 @@
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
 import ClientPage from "./ClientPage"
+import { prisma } from "@/lib/prisma"
 
 export default async function Page() {
   const session = await auth()
   if (!session?.user) redirect("/login") // TODO: 404로 redirect
+
+  const user = await prisma.user.findUnique({ where: { id: session.user.id } })
+  if (!user?.isAdmin) redirect("/")
 
   return <ClientPage />
 }
