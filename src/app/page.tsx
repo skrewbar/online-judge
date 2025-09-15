75b77a8dc15ba43aa435fc7bcf49e8815c2478bc
skrewbar <skurua1244@gmail.com>
2025-09-15 18:44:04 +0900

    ✨ 메인화면에 닉네임 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { auth } from "@/auth"
+
 export default async function Home() {
+  const session = await auth()
+  const user = session?.user
+
   return (
     <main className="flex flex-col w-1/2 m-auto text-center">
-      <h1 className="text-3xl">안녕하세요</h1>
-      <p>안녕하다는뜻</p>
+      <h1 className="text-3xl">안녕하세요, {user && user.handle}님!</h1>
     </main>
   )
 }
